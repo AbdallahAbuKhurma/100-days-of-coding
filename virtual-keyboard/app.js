e952e6742a94fb616684ff2d51bc64e39a7fbbdd
Abdallah Abu Khurma <79832840+AbdallahAbuKhurma@users.noreply.github.com>
2022-05-31 09:05:02 +0300

    initial submit

---
+++ virtual-keyboard/app.js
@@ -0,0 +1,54 @@
+var $keyboardWrapper = $('.virtual-keyboard'),
+    $key = $keyboardWrapper.find('input'),
+    $key_delete = $('.delete'),
+    $key_shift = $('.shift'),
+    $outputField = $('.output input'),
+    $currentValue = $outputField.val(),
+    actionKey = $('.delete, .shift')
+    activeShiftClass = 'shift-activated';
+
+function _keystroke (keyCase) {
+    $key.not(actionKey).on('click', function (e) {
+        e.preventDefault();
+        // check for the shift key
+        if($key_shift.hasClass(activeShiftClass)) {
+            keyCase = 'upper';
+            $key_shift.removeClass(activeShiftClass);
+        } else {
+            keyCase = 'lower';
+        }
+        //handle case
+        if(keyCase == 'upper') {
+            var keyVal = $(this).val().toUpperCase();
+        } else {
+            var keyVal = $(this).val().toLowerCase();
+        }
+        //grap current value
+        var output = $('.output input').val();
+        $outputField.val(output + keyValue);
+        getCurrentValue();
+        focusOutputField();
+    });
+}
+
+$key_delete.on('click', function (e) {
+    e.preventDefault();
+    $outputField.val($currentValue.substr(0, $currentValue.length -1));
+    getCurrentValue();
+    focusOutputField();
+});
+
+$key_shift.on('click', function (e) {
+    e.preventDefault();
+    $(this).toggleClass(activeShiftClass);
+});
+
+function getCurrentValue(){
+    $currentValue = $outputField.val();
+}
+
+function focusOutputField(){
+    $outputField.focus();
+}
+
+_keystroke('lower');
